3e71d5bfccda5f35bf8993c6a4bfb0877c740b00
Sean Yeoh <sysxdev@protonmail.com>
2026-02-15 17:13:29 +0800

    Update neotree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,12 +12,18 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', ':Neotree focus<CR>', desc = 'Explorer NeoTree (Focus)', silent = true },
   },
   opts = {
     filesystem = {
+      bind_to_cwd = true, -- THIS syncs the terminal's directory with the tree
+      follow_current_file = {
+        enabled = true, -- THIS makes the tree jump to the file you are editing
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['<bs>'] = 'noop', -- "noop" means No Operation (does nothing)
         },
       },
     },
